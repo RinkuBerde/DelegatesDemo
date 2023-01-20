ccd184a627c056c7770498cce109ed1c5ef2307f
rinkuberde <rinkuberde@gmail.com>
2023-01-21 00:00:40 +0530

    [Rinku] - Welcome Message

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DelegatesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Delegates Demo!!!");
+        }
+    }
+}
