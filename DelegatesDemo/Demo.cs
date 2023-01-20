bec6fa50343a8066baeb538cc283bda8ea51d800
rinkuberde <rinkuberde@gmail.com>
2023-01-21 00:07:38 +0530

    [Rinku] - singlecast and Multicast delegates

---
+++ DelegatesDemo/Demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    internal class Demo
+    {
+        public delegate int operation(int x, int y);
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+
--- DelegatesDemo/Program.cs
+++ DelegatesDemo/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace DelegatesDemo
+﻿using static DelegatesDemo.Demo;
+
+namespace DelegatesDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Delegates Demo!!!");
+            //operation obj = new operation(Demo.Addition);
+            //Console.WriteLine("Addition is = {0}", obj(23, 27));
+             MulticastDelegate.ImplementDelegate();
+            Console.ReadKey();
         }
     }
+    
 }
